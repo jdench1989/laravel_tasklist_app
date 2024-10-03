bfa5693af75586a1d89ffa05fe1ac2e90cafcb29
Jack Dench <jdench1989@gmail.com>
2024-10-03 12:43:56 +0100

    Sets up Projects and Tasks seeders

--- database/seeders/ProjectsSeeder.php
+++ database/seeders/ProjectsSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class ProjectsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        for ($i = 1; $i <= 3; $i++) {
+            Project::create([
+                'name' => "Project $i",
+            ]);
+        }
     }
 }
